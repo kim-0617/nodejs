87db0d05d03e4392b2a5fa872aa2b32a9a1a3f21
kim-0617 <kkk5993@naver.com>
2022-08-07 23:37:46 +0900

    22.08.07
    23:37, permanent cookie

--- cookies/cookie.js
+++ cookies/cookie.js
@@ -1,10 +1,18 @@
 const http = require('http');
+const cookie = require('cookie');
 
 http.createServer((request, response)=>{
-    response.writeHead(200, {
-        'Set-Cookie' : ['yummy_cookie=choko', 'tasty_cookie=strawberry']
-    });
-
-    console.log(request.headers.cookie);
-    response.end('Cookie!');
+    // response.writeHead(200, {
+    //     'Set-Cookie' : [
+    //         'yummy_cookie=choko', 
+    //         'tasty_cookie=strawberry',
+    //         `Permanent=cookies; Max-Age = ${60*60*24*3}`,
+    //     ]
+    // });
+    let cookies = {};
+    if(request.headers.cookie !== undefined){
+        cookies = cookie.parse(request.headers.cookie);
+    }
+    console.log(cookies);
+    response.end("cookies");
 }).listen(3000);
