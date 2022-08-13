b9384cdbaf72359bef7192d2089984dae39e28eb
kim-0617 <kkk5993@naver.com>
2022-08-14 00:59:52 +0900

    22.08.14
    12:59

--- cookies/cookie.js
+++ cookies/cookie.js
@@ -2,17 +2,20 @@ const http = require('http');
 const cookie = require('cookie');
 
 http.createServer((request, response)=>{
-    // response.writeHead(200, {
-    //     'Set-Cookie' : [
-    //         'yummy_cookie=choko', 
-    //         'tasty_cookie=strawberry',
-    //         `Permanent=cookies; Max-Age = ${60*60*24*3}`,
-    //     ]
-    // });
     let cookies = {};
     if(request.headers.cookie !== undefined){
         cookies = cookie.parse(request.headers.cookie);
     }
     console.log(cookies);
+    response.writeHead(200, {
+        'Set-Cookie' : [
+            // 'yummy_cookie=choko', 
+            // 'tasty_cookie=strawberry',
+            // `Permanent=cookies; Max-Age = ${60*60*24*3}`,
+            // `Secure=Secure; Secure`,
+            // `HttpOnly=HttpOnly; HttpOnly`,
+            // `Path=Path; Path=/cookie`,
+        ]
+    });
     response.end("cookies");
 }).listen(3000);
--- main.js
+++ main.js
@@ -8,13 +8,24 @@ connection.connect();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('images')); // images 폴더의 정적인 파일들을 서비스 가능하게 된다.
+app.get('/null', (request, response) => response.status(204));
 app.get('/page/null', (request, response) => response.status(204));
 app.get('/create/null', (request, response) => response.status(204));
 app.get('/update/null', (request, response) => response.status(204));
+app.get('/author/update/null', (request, response) => response.status(204));
 app.get('/favicon.ico', (req, res) => res.status(204));
 
 app.get('/', (request, response) => {
 	topic.home(request, response);
+});
+app.get('/login', (request, response) => {
+	topic.login(request, response);
+});
+app.post('/login_process', (request, response) => {
+	topic.login_process(request, response);
+});
+app.get('/logout_process', (request, response) => {
+	topic.logout_process(request, response);
 });
 app.get('/page/:id', (request, response) => {
 	topic.page(request, response);
@@ -34,6 +45,24 @@ app.post('/update_process', (request, response) => {
 app.post('/delete_process', (request, response) => {
 	topic.delete_process(request, response);
 });
+app.get('/author', (request, response) => {
+	author.home(request, response);
+});
+app.get('/author/create', (request, response) => {
+	author.create(request, response);
+});
+app.get('/author/update/:update_id', (request, response) => {
+	author.update(request, response);
+});
+app.post('/author/create_process', (request, response) => {
+	author.create_process(request, response);
+});
+app.post('/author/update_process', (request, response) => {
+	author.update_process(request, response);
+});
+app.post('/author/delete_process', (request, response) => {
+	author.delete_process(request, response);
+});
 
 app.use((request, response, next) => {
 	response.status(404).send("Sorry, can't find that");
@@ -42,13 +71,13 @@ app.use((request, response, next) => {
 // 어떤 미들웨어에서 next의 인자로 err가 넘어오게 되면, next의 다음 동작으로 수행되는 미들웨어는
 // 인자가 4개인 미들웨어를 호출하도록 약속되어 있다.
 // 따라서 여기에 Error Handling 을 하면 된다.
-app.use((err, request, response, next) => { 
-	console.err(err.stack);
-	response.status(500).send("Something Broke");
-})
+// app.use((err, request, response, next) => { 
+// 	console.err(err.stack);
+// 	response.status(500).send("Something Broke");
+// });
 
 app.listen(3000, () =>{
-	console.log("example app listening on port 3000!");
+	console.log("example app listening on port 3000~");
 });
 /*
 const http = require('http');
