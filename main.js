932042ca77d4500c8f059fc8788d8e61b3c6843d
kim-0617 <kkk5993@naver.com>
2022-08-15 18:03:11 +0900

    22.08.15
    18:03

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const topic = require('./lib/topic.js');
 const author = require('./lib/author.js');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const flash = require('connect-flash');
 connection.connect();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,18 +26,87 @@ app.use(session({
 	store: new FileStore(),
 }));
 
+app.use(flash());
+
+const authData = {
+	email: `egoing777@gmail.com`,
+	password: `111111`,
+	nickname: `egoing`,
+}
+
+// 세션 코드 다음에 위치해야 됩니다.
+const passport = require('passport')
+	, LocalStrategy = require('passport-local').Strategy; // 1. passport 설치
+
+app.use(passport.initialize());
+app.use(passport.session()); // 2. express에 설치 (미들웨어)
+
+passport.serializeUser((user, done) => {
+	done(null, user.email);
+}); // 로그인 성공 시 세션스토어에 저장하는 방법 (email만 세션에 넘어간다)
+passport.deserializeUser((id, done) => {
+	done(null, authData);
+}); // 페이지 방문할 때마다 세션스토어에 있는 식별자를 가져와서 데이터를 사용할 방법 정의
+
+passport.use(new LocalStrategy(
+	{
+		usernameField: `email`,
+		passwordField: `password`,
+	}, // 필드 커스터마이징
+
+	function (username, password, done) {
+		if (username === authData.email) {
+			if (password === authData.password) {
+				return done(null, authData);
+			}
+			else {
+				return done(null, false, {
+					message: `Incorrect password.`
+				});
+			}
+		}
+		else {
+			return done(null, false, {
+				message: `Incorrect username.`
+			});
+		}
+	}
+)); // 로그인 성공 여부 판단
+
+app.post(`/login_process`,
+	passport.authenticate(`local`, {
+		successRedirect: `/`,
+		failureRedirect: `/login`,
+	})
+); // 데이터를 받았을 때 어떻게 처리할 것인가
+app.get('/logout_process', (request, response) => {
+	request.logout(function (err) {
+		if (err) { return next(err); }
+		// request.session.destroy((err) => {
+		// 	response.redirect('/');
+		// });
+		request.session.save(() => {
+			response.redirect('/');
+		})
+	});
+});
+
 app.get('/', (request, response) => {
 	topic.home(request, response);
 });
 app.get('/login', (request, response) => {
 	topic.login(request, response);
 });
+/*
 app.post('/login_process', (request, response) => {
 	topic.login_process(request, response);
 });
+*/
+/*
 app.get('/logout_process', (request, response) => {
 	topic.logout_process(request, response);
 });
+*/
 app.get('/page/:id', (request, response) => {
 	topic.page(request, response);
 });
